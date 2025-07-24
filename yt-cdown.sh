0dc0be107133a7d8b39bc56735a91eeeff28be90
lordmiller94 <41526402+lordmiller94@users.noreply.github.com>
2025-07-24 14:32:12 -0300

    Update yt-cdown.sh

--- yt-cdown.sh
+++ yt-cdown.sh
@@ -3,7 +3,7 @@
 #This script try to download all youtube videos from any channel
 #Recommend: put this script in empty folder before run!!!!
 #Requiriments: yt-dlp;jq;ffmpeg;tr
-#Usage: script.sh YOUTUBE_CHANNEL_URL
+#Usage: yt-cdown.sh YOUTUBE_CHANNEL_URL
 #Tested on ubuntulinux based systems
 
 ####VARIABLES####
